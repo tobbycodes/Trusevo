737b9291075a453db43dece90800d62049761aba
Oluwatobi Adewole <tobiadewole565@gmail.com>
2026-07-29 19:15:51 +0100

    update

--- src/App.tsx
+++ src/App.tsx
@@ -397,11 +397,7 @@ export default function App() {
               </div>
             </div>
 
-            {/* Copyright */}
-            <div className="pt-10 text-xs text-gray-600 font-body">
-              <span>&copy; {new Date().getFullYear()} Trusevo Studio. All rights reserved.</span>
-            </div>
-          </div>
+           
 
           {/* Right Column: Dynamic collaboration representation */}
           <div className="space-y-8 text-left w-full">
@@ -439,6 +435,11 @@ export default function App() {
                 X
               </a>
             </div>
+             {/* Copyright */}
+            <div className="pt-10 text-xs text-gray-600 font-body">
+              <span>&copy; {new Date().getFullYear()} Trusevo Studio. All rights reserved.</span>
+            </div>
+          </div>
           </div>
 
         </div>
